caf6360f84096db6ec4bbcda2e79cf4baf747e61
Ixas-5 <isabellanahir@gmail.com>
2025-08-25 19:05:00 -0400

    Añadi un website

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,16 +3,16 @@ export const projects = [
   {
     id: 1,
     title: {
-      en: "",
-      es: "",
+      en: "Smart Consumption",
+      es: "Consumo Inteligente",
     },
     description: {
-      en: "",
-      es: "",
+      en: "This is a website dedicated to help local Puerto Rican farmers sell their products locally.",
+      es: "Esto es un website dedicado a ayudar agricultores Puertorriqueños vender sus productos localmente.",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://consumo-inteligente.vercel.app/",
+    githubRepo: "https://github.com/Ixas-5/Consumo-Inteligente",
   },
   // Add more projects here...
 ];
